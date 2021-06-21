fe0890b3899b1c91e12ef5acb67beb6459149069
cristiano-ribeiro-oleconsignado <85969978+cristiano-ribeiro-oleconsignado@users.noreply.github.com>
2021-06-21 20:38:45 -0300

    Update cicd.sh

--- cicd.sh
+++ cicd.sh
@@ -4,17 +4,13 @@ set -e
 
 CICD_COMMON_VERSION="v2.2"
 
-export CLASS_LIBRARY_PROJ_DIR=Source/Otc.ExceptionHandling.Abstractions
+export CLASS_LIBRARY_PROJ_DIR=Source/Otc.ExceptionHandling
 export TEST_PROJ_DIR=Source/Otc.ExceptionHandling.Tests
 
 cd $TRAVIS_BUILD_DIR
 
 wget -q https://raw.githubusercontent.com/OleConsignado/otc-cicd-common/$CICD_COMMON_VERSION/cicd-common.sh -O ./cicd-common.sh
 chmod +x ./cicd-common.sh
-
-./cicd-common.sh $@
-
-export CLASS_LIBRARY_PROJ_DIR=Source/Otc.ExceptionHandling
 
 ./cicd-common.sh $@
 
